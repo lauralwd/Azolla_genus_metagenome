cfd4f7d4ba7b8af91d70a30f21250e92ead57260
Laura Dijkhuizen <laura.w.dijkhuizen@gmail.com>
2019-03-05 12:12:19 +0100

    bash script to summarise contig/scaffold length+cov+taxonomy in a loop resulting in one compressed output summary.

---
+++ scripts/make_assembly_stats_and_taxonomy.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This script takes tabular CAT taxonomy files derived from both contigs and scaffolds ($files) of
+# several assemblies ($assemblytype) based on several sequencing libraries($hostcode).
+# with these tabular files, it creates one summary containing stats coded in the SPAdes header
+# after assembly, and the taxonomy added to these long sequences by CAT (Contig Annotation Tool)
+# This script seamlessly fits in with the snakemake workflow it came with.
+
+# Let's get the variables from the commandline
+assemblytype=$1
+hostcodes=$2
+files=$3
+CPU=$4
+MEM=$5
+output=$6
+
+# run a loop over the input variables given to this script by snakemake wildcards
+for   a in ${assemblytype[@]}
+do    for   h in ${hostcodes[@]}
+      do    for   f in ${files[@]}
+            do    grep -v '#' "./data/$a/$h/"CAT_"$h"_"$f"_taxonomy.tab | tr '_' "\t" | cut -f 2-  | sort -k1n --parallel $CPU -s $mem | sed  "s/^/$a\t$h\t$f\t/g"
+      done
+done | cut -f 1,2,3,5,7,8,13- | cut -f 1-17 | pigz -c -p {threads} -R > ""$output"
